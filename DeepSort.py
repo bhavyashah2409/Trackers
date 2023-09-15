878cb46dca36ee7e1d4a8c345419e578c5038371
bhavyashah2409 <bhavya@accurateic.in>
2023-09-15 12:56:06 +0530

    Added original DeepSort

---
+++ DeepSort.py
@@ -0,0 +1,78 @@
+################# UNCOMMENT IF WANT TO USE DUPLICATE DEEPSORT LIBRARY
+
+# import cv2 as cv
+# from ultralytics import YOLO
+# from deep_sort_realtime.deepsort_tracker import DeepSort
+
+# MAX_AGE = 60
+
+# video = r"C:\Users\aicpl\ShipsDatasets\VideoDataset\videos\video_24.mp4"
+# cap = cv.VideoCapture(video)
+
+# model = YOLO('best.pt')
+# tracker = DeepSort(max_age=MAX_AGE)
+
+# while True:
+#     ret, frame = cap.read()
+#     detections = model.predict(frame)[0]
+#     results = []
+#     for xmin, ymin, xmax, ymax, p, c in detections.boxes.data.tolist():
+#         results.append([[int(xmin), int(ymin), int(xmax - xmin), int(ymax - ymin)], p, c])
+#     tracks = tracker.update_tracks(results, frame=frame)
+#     for track in tracks:
+#         if track.is_confirmed():
+#             i = track.track_id
+#             c = track.get_det_class()
+#             xmin, ymin, xmax, ymax = track.to_ltrb(orig=True)
+#             frame = cv.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
+#             p = track.get_det_conf()
+#             if p is not None:
+#                 frame = cv.putText(frame, f'ID: {i}, {c}: {round(p * 100, 2)}', (int(xmin), int(ymin - 10)), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+#             else:
+#                 frame = cv.putText(frame, f'ID: {i}, {c}', (int(xmin), int(ymin - 10)), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+#     cv.imshow('Frame', frame)
+#     cv.waitKey(1)
+# cap.release()
+
+################################ ORIGINAL YOLO + DEEPSORT
+
+import cv2 as cv
+from ultralytics import YOLO
+from deep_sort_pytorch.deep_sort import DeepSort
+
+VIDEO = r"C:\Users\aicpl\ShipsDatasets\CollisionDetection\test6.mp4"
+WEIGHTS = 'yolov8l.pt'
+
+model = YOLO(WEIGHTS)
+deepsort = DeepSort(
+    model_path=r'C:\Users\aicpl\ShipsDatasets\DeepSort\YOLOv8-DeepSORT-Object-Tracking\ultralytics\yolo\v8\detect\deep_sort_pytorch\deep_sort\deep\checkpoint\ckpt.t7',
+    max_dist=0.2,
+    min_confidence=0.3,
+    nms_max_overlap=0.5,
+    max_iou_distance=0.7,
+    max_age=70,
+    n_init=5,
+    nn_budget=100,
+    use_cuda=True
+    )
+cap = cv.VideoCapture(VIDEO)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    preds = model.predict(frame)[0].cpu()
+    classes = preds.names
+    preds = preds.boxes
+    xywhs = preds.xywh
+    confs = preds.conf
+    oids = preds.cls
+    final = deepsort.update(xywhs, confs, oids, frame)
+    if len(final) > 0:
+        for xmin, ymin, xmax, ymax, i, c in final:
+            frame = cv.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+            frame = cv.putText(frame, f'ID: {i}, {classes[c]}', (xmin, ymin + 20), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+    cv.imshow('Frame', frame)
+    cv.waitKey(1)
+
+cap.release()
---
+++ TrackingYOLO.py
@@ -0,0 +1,20 @@
+import cv2
+from ultralytics import YOLO
+
+# Load the YOLOv8 model
+model = YOLO('best.pt')
+video_path = r'C:\Users\aicpl\ShipsDatasets\VideoDataset\videos\video_24.mp4'
+cap = cv2.VideoCapture(video_path)
+
+while cap.isOpened():
+    success, frame = cap.read()
+    if success:
+        results = model.track(frame, conf=0.65, iou=0.8, persist=True)
+        annotated_frame = results[0].plot()
+        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
